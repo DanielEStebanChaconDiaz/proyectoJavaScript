d86264adcb361f29f84cf0270af275358223603c
DanielEStebanChaconDiaz <138712462+DanielEStebanChaconDiaz@users.noreply.github.com>
2024-05-14 11:53:58 -0500

    feat: :construction: creacion de la reproduccion por album
    Se logro reproducir por album

--- js/main.js
+++ js/main.js
@@ -1,82 +1,80 @@
-class myframe extends HTMLElement{
+class MyFrame extends HTMLElement {
     id
-    constructor(id){
+
+    constructor() {
         super();
-        this.attachShadow({mode: "open"});
+        this.attachShadow({ mode: "open" });
     }
-    connectedCallback(){
-        this.shadowRoot.innerHTML = /*html*/`
-            <iframe class="spotify-iframe" width="454" height="690" src="https://open.spotify.com/embed/album/${this.id}" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-        `
+    
+    connectedCallback() {
+        // Este método se llama cuando el elemento se conecta al DOM
+        // Aquí puedes inicializar elementos, establecer eventos, etc.
     }
-    static get observedAttributes(){
+
+    static get observedAttributes() {
         return ["uri"];
     }
-    attributeChangedCallback(name,old,now){
-        let[nameUri, album, id] = now.split(":")
+
+    attributeChangedCallback(name, oldVal, newVal) {
+        let [, , id] = newVal.split(":");
         this.id = id;
+        this.shadowRoot.innerHTML = `
+            <iframe class="spotify-iframe" width="454" height="690" src="https://open.spotify.com/embed/album/${this.id}" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+        `;
     }
 }
-customElements.define("my-frame",myframe)
 
-const url = 'https://spotify23.p.rapidapi.com/search/?q=%3CREQUIRED%3E&type=album&offset=0&limit=10&numberOfTopResults=5';
+customElements.define("my-frame", MyFrame);
+
+let code = "%3CREQUIRED%3E"
+code.replace(" ", "%20")
+console.log(code);
+
+
+const url = `https://spotify23.p.rapidapi.com/search/?q=${code}&type=album&offset=0&limit=10&numberOfTopResults=5`;
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '6275bafd33mshb8de4d750097a4fp1e70bcjsne2bdaa25c18e',
-		'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
-	}
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '6275bafd33mshb8de4d750097a4fp1e70bcjsne2bdaa25c18e',
+        'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+    }
 };
 
 try {
-	const response = await fetch(url, options);
-	const result = await response.json();
-    let va = result.albums.items
-    for(let i = 0; i < 1000; i++){
-        fetch (url)
-        .then(response => response.json())
-        .then(data => mostrarAlbum(data))
-        .then(data => escucharAlbum(data))
-        const getImage = result.albums.items[i]?.data.coverArt.sources[i]?.url;
-        const firstImage = result.albums.items[i]?.data.coverArt.sources[0]?.url;
+    const response = await fetch(url, options);
+    const result = await response.json();
+    let va = result.albums.items;
 
+    for(let i = 0; i < va.length; i++) {
+        const getImage = va[i]?.data.coverArt.sources[i]?.url;
+        const firstImage = va[i]?.data.coverArt.sources[0]?.url;
         let imagen = getImage ?? firstImage;
-        let nombre = result.albums.items[i].data.name
-        let nombreArtista = result.albums.items[i].data.artists.items[i]?.profile.name ?? result.albums.items[i].data.artists.items[0]?.profile.name;
-        let fecha = result.albums.items[i].data.date.year
-        let uri = result.albums.items[i].data.uri
+        let nombre = va[i].data.name
+        let nombreArtista = va[i].data.artists.items[i]?.profile.name ?? va[i].data.artists.items[0]?.profile.name;
+        let fecha = va[i].data.date.year
+        let uri = va[i].data.uri
         
-        const mostrarAlbum = async () => {
-            const searchHeader = document.querySelector(".search-header")
-            const div = document.createElement("fragment");
-            div.classList.add("album")
-            searchHeader.after += `
-            <div class="album">
-                <div class="album_order" data-id="${uri}">
-                    <div class="imagen_album">
-                        <img src="${imagen}" alt="" class="portada"">
-                    </div>
-                    <div class="info_album">
-                        <h3>${nombre}</h3>
-                        <p>${nombreArtista}</p>
-                        <p>${fecha}</p>
-                    </div>
+        const div = document.createElement("div");
+        div.classList.add("album")
+        div.innerHTML =`
+        <div class="album">
+            <div class="album_order" data-id="${uri}">
+                <div class="imagen_album">
+                    <img src="${imagen}" alt="" class="portada"">
                 </div>
-            </div>`;
-            listarAlbum.append(div);
-        }
-        const escucharAlbum = async () => {
-            const div =document.createElement("div");
-            div.classList.add("frame");
-            div.innerHTML=`
-            <my-frame uri="${uri}" class="frame"></my-frame>`
-            listarAlbum.append(div)
-        }
+                <div class="info_album">
+                    <h3>${nombre}</h3>
+                    <p>${nombreArtista}</p>
+                    <p>${fecha}</p>
+                </div>
+            </div>
+        </div>`;
+        listarAlbum.append(div);
+        div.querySelector('.album_order').addEventListener('click', () => {
+            const frame = document.querySelector("my-frame");
+            frame.setAttribute("uri", uri);
+        });
     }
 } catch (error) {
-	console.error(error);
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-    mostrarAlbum();
-})
+    console.error(error);
+};
