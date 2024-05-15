897d14fa72617c3d616058f9bdc5364e804b5bab
DanielEStebanChaconDiaz <138712462+DanielEStebanChaconDiaz@users.noreply.github.com>
2024-05-15 07:29:29 -0500

    feat: :construction: Inicio de las tracks recomendadas
    Se creo el inicio del codigo para tracks recomendadas

--- js/main.js
+++ js/main.js
@@ -101,3 +101,41 @@ async function mostrarAlbums(code) {
         console.error(error);
     }
 }
+const url = 'https://spotify23.p.rapidapi.com/recommendations/?limit=20&seed_tracks=0c6xIDDpzE81m2q797ordA&seed_artists=4NHQUGzhtTLFvgF5SZesLK&seed_genres=classical%2Ccountry';
+const options = {
+    method: 'GET',
+	headers: {
+        'X-RapidAPI-Key': '6275bafd33mshb8de4d750097a4fp1e70bcjsne2bdaa25c18e',
+		'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+	}
+};
+try {
+	const response = await fetch(url, options);
+	const result = await response.json();
+    const tracks = result.tracks.items;
+    console.log(tracks)
+    for (let i = 0; i < 10; i++) {
+        const getImage = tracks[i]?.albums.images.url;
+        const firstImage = tracks[i]?.albums.images.url;
+        const imagen = getImage ?? firstImage;
+        const nombre = tracks[i].name;
+        const nombreArtista = tracks[i].artists.items[0].name
+        const uri = tracks[i].uri;
+
+        const div = document.createElement("div");
+        div.classList.add("trackRecomendations");
+        div.innerHTML = `
+            <div class="track_order" data-id="${uri}">
+                <div class="imagen_track">
+                    <img src="${imagen}" alt="" class="portada">
+                </div>
+                <div class="info_track">
+                    <h3>${nombre}</h3>
+                    <p>${nombreArtista}</p>
+                </div>
+            </div>
+        `;
+    }
+} catch (error) {
+	console.error(error);
+}
