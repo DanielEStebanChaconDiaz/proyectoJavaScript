aed1e7f4820d42fc5486c7d164913ac9e9a075f2
DanielEStebanChaconDiaz <138712462+DanielEStebanChaconDiaz@users.noreply.github.com>
2024-05-15 13:06:56 -0500

    feat: :construction: Implementacion de playlist
    Se implementaron las playlist y las tracks

--- js/main.js
+++ js/main.js
@@ -17,13 +17,20 @@ class MyFrame extends HTMLElement {
 
     attributeChangedCallback(name, oldVal, newVal) {
         let [, , id] = newVal.split(":");
+        const uri = this.getAttribute("uri")
+        const type = uri.split(":")[1]
         this.id = id;
+        this.type = type;
         this.shadowRoot.innerHTML = `
-            <iframe class="spotify-iframe" width="454" height="690" src="https://open.spotify.com/embed/album/${this.id}" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+            <iframe class="spotify-iframe" width="100%" height="750" src="https://open.spotify.com/embed/${this.type}/${this.id}" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
         `;
+        if (type == "track"){
+            this.shadowRoot.innerHTML = `
+            <iframe class="spotify-iframe" width="50%" height="400" src="https://open.spotify.com/embed/${this.type}/${this.id}" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+        `;
+        }
     }
-}
-
+};
 customElements.define("my-frame", MyFrame);
 
 const searchInput = document.querySelector('.search-header__input');
@@ -54,14 +61,14 @@ searchInput.addEventListener('keypress', (e) => {
 });
 
 async function mostrarAlbums(code) {
-    let url = `https://spotify23.p.rapidapi.com/search/?q=${code}&type=album&offset=0&limit=10&numberOfTopResults=5`;
+    let url = `https://spotify23.p.rapidapi.com/search/?q=${code}&type=albums&offset=0&limit=10&numberOfTopResults=5`;
     const options = {
         method: 'GET',
         headers: {
-            'X-RapidAPI-Key': '6275bafd33mshb8de4d750097a4fp1e70bcjsne2bdaa25c18e',
+            'X-RapidAPI-Key': '60c0b8f59bmshb8d3920516e1c27p15151djsn79cfdb969902',
             'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
         }
-    };
+};
 
     try {
         const response = await fetch(url, options);
@@ -100,28 +107,28 @@ async function mostrarAlbums(code) {
     } catch (error) {
         console.error(error);
     }
-}
+};
 const url = 'https://spotify23.p.rapidapi.com/recommendations/?limit=20&seed_tracks=0c6xIDDpzE81m2q797ordA&seed_artists=4NHQUGzhtTLFvgF5SZesLK&seed_genres=classical%2Ccountry';
 const options = {
-    method: 'GET',
+	method: 'GET',
 	headers: {
-        'X-RapidAPI-Key': '6275bafd33mshb8de4d750097a4fp1e70bcjsne2bdaa25c18e',
+		'X-RapidAPI-Key': '60c0b8f59bmshb8d3920516e1c27p15151djsn79cfdb969902',
 		'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
 	}
 };
+
 try {
 	const response = await fetch(url, options);
 	const result = await response.json();
-    const tracks = result.tracks.items;
-    console.log(tracks)
+    const tracks = result.tracks;
     for (let i = 0; i < 10; i++) {
-        const getImage = tracks[i]?.albums.images.url;
-        const firstImage = tracks[i]?.albums.images.url;
-        const imagen = getImage ?? firstImage;
+        const img = tracks[i]?.album.images[0]?.url;
+        const img2 = tracks[i]?.album.images[i]?.url;
+        const imagen = img ?? img2;
         const nombre = tracks[i].name;
-        const nombreArtista = tracks[i].artists.items[0].name
+        const nombreArtista = tracks[i].artists[0].name
         const uri = tracks[i].uri;
-
+        console.log(img)
         const div = document.createElement("div");
         div.classList.add("trackRecomendations");
         div.innerHTML = `
@@ -135,6 +142,51 @@ try {
                 </div>
             </div>
         `;
+        listarTrack.append(div);
+        div.querySelector('.track_order').addEventListener('click', () => {
+            const frame = document.querySelector("my-frame");
+            frame.setAttribute("uri", uri);
+        });
+    }
+} catch (error) {
+	console.error(error);
+};
+const url2 = 'https://spotify23.p.rapidapi.com/playlist_tracks/?id=37i9dQZF1DX4Wsb4d7NKfP&offset=0&limit=100';
+const options2 = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '60c0b8f59bmshb8d3920516e1c27p15151djsn79cfdb969902',
+		'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+	}
+};
+
+try {
+	const response2 = await fetch(url2, options2);
+	const result2 = await response2.json();
+	const playlist = result2.items;
+    for (let i = 0; i < 10; i++) {
+        const img = playlist[i]?.track.album?.images[0].url;
+        const imagen = img;
+        const nombre = playlist[i].track.album.name;
+        const uri = playlist[i].track.album.uri;
+        console.log(img)
+        const div = document.createElement("div");
+        div.classList.add("PlayList");
+        div.innerHTML = `
+            <div class="track_order" data-id="${uri}">
+                <div class="imagen_playlist">
+                    <img src="${imagen}" alt="" class="portada">
+                </div>
+                <div class="info_track">
+                    <h3>${nombre}</h3>
+                </div>
+            </div>
+        `;
+        listarPlayList.append(div);
+        div.querySelector('.track_order').addEventListener('click', () => {
+            const frame = document.querySelector("my-frame");
+            frame.setAttribute("uri", uri);
+        });
     }
 } catch (error) {
 	console.error(error);
