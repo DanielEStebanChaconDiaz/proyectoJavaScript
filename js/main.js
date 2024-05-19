e058fad48d49145944c6a49f6dbd9d8ac327834a
DanielEStebanChaconDiaz <138712462+DanielEStebanChaconDiaz@users.noreply.github.com>
2024-05-19 11:10:52 -0500

    feat: :recycle: Refactorizacion de codigo
    Se refactorizo el codigo para agregar nuevas funciones

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ class MyFrame extends HTMLElement {
         this.attachShadow({ mode: "open" });
     }
 
-    connectedCallback() {}
+    connectedCallback() { }
 
     static get observedAttributes() {
         return ["uri"];
@@ -63,7 +63,7 @@ async function mostrarAlbums(code) {
     const options = {
         method: 'GET',
         headers: {
-            'X-RapidAPI-Key': '2057b038e1msha82feb3472c16f6p1bc6a8jsn67d5759cb0ea',
+            'X-RapidAPI-Key': 'f39f63f611msh8bc0f870d3507dap1ccbeajsn30cb22305b23',
             'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
         }
     };
@@ -98,8 +98,8 @@ async function mostrarAlbums(code) {
             `;
             listarAlbum.append(div);
             div.querySelector('.album_order').addEventListener('click', async () => {
-                await reproducirPrimerTrack(uri);
-                mostrarTracks(uri);
+                await reproducirPrimerTrack(uri); // Espera a que se reproduzca el primer track
+                mostrarTracks(uri); // Mostrar los tracks del álbum
             });
         }
     } catch (error) {
@@ -107,14 +107,13 @@ async function mostrarAlbums(code) {
     }
 }
 
-
-async function reproducirPrimerTrack(albumUri){
+async function reproducirPrimerTrack(albumUri) {
     let albumId = albumUri.split(":")[2];
     let url = `https://spotify23.p.rapidapi.com/albums/?ids=${albumId}`;
     const options = {
         method: 'GET',
         headers: {
-            'X-RapidAPI-Key': '2057b038e1msha82feb3472c16f6p1bc6a8jsn67d5759cb0ea',
+            'X-RapidAPI-Key': 'f39f63f611msh8bc0f870d3507dap1ccbeajsn30cb22305b23',
             'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
         }
     };
@@ -123,9 +122,9 @@ async function reproducirPrimerTrack(albumUri){
         const response = await fetch(url, options);
         const result = await response.json();
         const tracks = result.albums[0].tracks.items;
-        const uri = tracks[0]?.uri; 
+        const uri = tracks[0]?.uri; // URI del primer track
         const frame = document.querySelector("my-frame");
-        frame.setAttribute("uri", uri); 
+        frame.setAttribute("uri", uri); // Establecer la URI del primer track
     } catch (error) {
         console.error(error);
     }
@@ -137,7 +136,7 @@ async function mostrarTracks(albumUri) {
     const options = {
         method: 'GET',
         headers: {
-            'X-RapidAPI-Key': '2057b038e1msha82feb3472c16f6p1bc6a8jsn67d5759cb0ea',
+            'X-RapidAPI-Key': 'f39f63f611msh8bc0f870d3507dap1ccbeajsn30cb22305b23',
             'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
         }
     };
@@ -152,14 +151,14 @@ async function mostrarTracks(albumUri) {
             const nombre = track.name;
             const nombreArtista = track.artists[0].name;
             const uri = track.uri;
-            const imagen = result.albums[0].images[0].url
+            const imagen = result.albums[0].images[0].url;
 
             const div = document.createElement("div");
             div.classList.add("track");
             div.innerHTML = `
                 <div class="track_order" data-id="${uri}">
                     <div class="imagen_track">
-                    <i class='bx bx-menu-alt-left' style='color:#1db954' ></i>
+                        <i class='bx bx-menu-alt-left' style='color:#1db954' ></i>
                         <img src="${imagen}" alt="" class="portada">
                     </div>
                     <div class="info_track">
@@ -179,62 +178,85 @@ async function mostrarTracks(albumUri) {
     }
 }
 
-const listarAlbum = document.querySelector('#listarAlbum');
+document.addEventListener('DOMContentLoaded', () => {
+    buscarTrack(code);
+});
+
+searchButton.addEventListener('click', () => {
+    const query = searchInput.value.trim();
+    if (query) {
+        const formattedQuery = query.replace(" ", "%20");
+        buscarTrack(formattedQuery);
+    }
+});
+
+searchInput.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        const query = searchInput.value.trim();
+        if (query) {
+            const formattedQuery = query.replace(" ", "%20");
+            buscarTrack(formattedQuery);
+        }
+    }
+});
+
 const listarTrack = document.querySelector('#listarTrack');
-const listarPlayList = document.querySelector('#listarPlayList');
 
-const urlRecommendations = `https://spotify23.p.rapidapi.com/recommendations/?limit=20&seed_tracks=0c6xIDDpzE81m2q797ordA&seed_artists=4NHQUGzhtTLFvgF5SZesLK&seed`
-const optionsRecommendations = {
-    method: 'GET',
-    headers: {
-        'X-RapidAPI-Key': '2057b038e1msha82feb3472c16f6p1bc6a8jsn67d5759cb0ea',
-        'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
-    }
-};
+async function buscarTrack(code) {
+    const urlRecommendations = `https://spotify23.p.rapidapi.com/search/?q=${code}&type=tracks&offset=0&limit=10&numberOfTopResults=5`;
+    const optionsRecommendations = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'f39f63f611msh8bc0f870d3507dap1ccbeajsn30cb22305b23',
+            'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+        }
+    };
 
-try {
-    const response = await fetch(urlRecommendations, optionsRecommendations);
-    const result = await response.json();
-    const tracks = result.tracks;
-    for (let i = 0; i < 10; i++) {
-        const img = tracks[i]?.album.images[0]?.url;
-        const img2 = tracks[i]?.album.images[i]?.url;
-        const imagen = img ?? img2;
-        const nombre = tracks[i].name;
-        const nombreArtista = tracks[i].artists[0].name;
-        const uri = tracks[i].uri;
-        const div = document.createElement("div");
-        div.classList.add("track_Recomendations");
-        div.innerHTML = `
-            <div class="track_order" data-id="${uri}">
-            <p class="num">${i+1}</p>
-            <i class='bx bx-play'></i>
-                <div class="imagen_track">
-                    <img src="${imagen}" alt="" class="portada">
+    try {
+        const response = await fetch(urlRecommendations, optionsRecommendations);
+        const result = await response.json();
+        const tracks = result.tracks.items;
+        listarTrack.innerHTML = '';
+        for (let i = 0; i < tracks.length; i++) {
+            const track = tracks[i].data;
+            const img = track.albumOfTrack.coverArt.sources[0].url;
+            const nombre = track.name;
+            const nombreArtista = track.artists.items[0].profile.name;
+            const uri = track.uri;
+
+            const div = document.createElement("div");
+            div.classList.add("track_Recomendations");
+            div.innerHTML = `
+                <div class="track_order" data-id="${uri}">
+                    <p class="num">${i + 1}</p>
+                    <i class='bx bx-play'></i>
+                    <div class="imagen_track">
+                        <img src="${img}" alt="" class="portada">
+                    </div>
+                    <div class="info_track">
+                        <h3>${nombre}</h3>
+                        <p>${nombreArtista}</p>
+                    </div>
                 </div>
-                <div class="info_track">
-                    <h3>${nombre}</h3>
-                    <p>${nombreArtista}</p>
-                </div>
-            </div>
-        `;
-        listarTrack.append(div);
-        div.querySelector('.track_order').addEventListener('click', () => {
-            const frame = document.querySelector("my-frame");
-            frame.setAttribute("uri", uri);
-        });
+            `;
+            listarTrack.append(div);
+            div.querySelector('.track_order').addEventListener('click', () => {
+                const frame = document.querySelector("my-frame");
+                frame.setAttribute("uri", uri);
+            });
+        }
+    } catch (error) {
+        console.error(error);
     }
-} catch (error) {
-    console.error(error);
 }
 
 const url = 'https://spotify23.p.rapidapi.com/search/?q=%3CREQUIRED%3E&type=playlists&offset=0&limit=10&numberOfTopResults=5';
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '2057b038e1msha82feb3472c16f6p1bc6a8jsn67d5759cb0ea',
-		'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
-	}
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'f39f63f611msh8bc0f870d3507dap1ccbeajsn30cb22305b23',
+        'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+    }
 };
 
 try {
@@ -249,7 +271,7 @@ try {
         div.classList.add("PlayList");
         div.innerHTML = `
             <div class="track_order" data-id="${uri}">
-            <p class="num">${i+1}</p>
+            <p class="num">${i + 1}</p>
             <i class='bx bx-play'></i>
                 <div class="imagen_playlist">
                     <img src="${imagen}" alt="" class="portada">
@@ -282,7 +304,7 @@ const trackOrders = document.querySelectorAll('.track_order');
 const iframe = document.querySelectorAll('.iframe');
 const movile = document.querySelectorAll('.menu__mobile');
 
-checkbox.addEventListener('change', function() {
+checkbox.addEventListener('change', function () {
     if (this.checked) {
         setStyles(containers, '#f3f3f3', '#191414');
         setStyles(search, 'white', '#191414');
@@ -314,27 +336,27 @@ checkbox.addEventListener('change', function() {
 });
 
 function setStyles(elements, backgroundColor, color) {
-    elements.forEach(function(element) {
+    elements.forEach(function (element) {
         element.style.background = backgroundColor;
         element.style.color = color;
     });
 }
 
 function resetStyles(elements) {
-    elements.forEach(function(element) {
+    elements.forEach(function (element) {
         element.style.background = '';
         element.style.color = '';
     });
 }
 
 function addHoverStyles(elements) {
-    elements.forEach(function(element) {
+    elements.forEach(function (element) {
         element.classList.add('hover-active');
     });
 }
 
 function removeHoverStyles(elements) {
-    elements.forEach(function(element) {
+    elements.forEach(function (element) {
         element.classList.remove('hover-active');
     });
 }
